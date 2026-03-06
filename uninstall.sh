33e8aaf00d6e15d11c2393b4c1fcf6ecf4661100
Lathif Baska <latieftjm@gmail.com>
2026-03-06 14:11:15 +0700

    update cleaner script

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+if [[ "$1" == "--uninstall" ]]; then
 echo "Menghapus bersihin..."
 
-rm -rf "$HOME/.bersihin"
-rm -f "$PREFIX/bin/bersihin"
+rm -rf ~/.bersihin
+rm -f $PREFIX/bin/bersihin
 
 echo "bersihin berhasil dihapus"
+exit
+fi
+
+echo "🧼 Mulai bersih-bersih Termux..."
+
+pip cache purge
+apt autoremove -y
+apt clean
+
+rm -rf ~/.cache/*
+rm -rf /data/data/com.termux/cache/*
+rm -rf /data/data/com.termux/files/usr/tmp/*
+rm -rf /data/data/com.termux/files/tmp/*
+
+find ~ -type d -name "__pycache__" -exec rm -r {} +
+
+echo "✅ Selesai auto-cleanup di $(date)"
